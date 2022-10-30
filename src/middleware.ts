dfa520f8964a9f4d4b9b7e06d327881ec504286d
DiLapiDev <giubertdilapi@example.com>
2022-10-30 00:50:52 -0300

    initial

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-import {IMovie} from "./components/MovieList/MovieList";
+import {IBike} from "./components/BikeList/BikeList";
 
 
 // This middleware checks if the movie requested in /movies/:id exists.
@@ -8,18 +8,18 @@ import {IMovie} from "./components/MovieList/MovieList";
 export async function middleware (request: NextRequest) {
   const urlItems = request.nextUrl.pathname.split('/').filter((item) => item !== '');
 
-  if (urlItems[0] === 'movies' && urlItems.length === 2) {
-    const movieId = urlItems[1];
+  if (urlItems[0] === 'bikes' && urlItems.length === 2) {
+    const bikeid = urlItems[1];
 
     // TODO: Improve perfomance. Fetching movies every time we make this check
     //  is too expensive.
-    const movies: IMovie[] = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies`)
+    const bikes: IBike[] = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/bikes`)
       .then((res) => res.json())
-      .then((data: IMovie[]) => data.slice(0, 20))
-      .catch((error) => console.error(error)) as IMovie[];
+      .then((data: IBike[]) => data.slice(0, 6))
+      .catch((error) => console.error(error)) as IBike[];
 
-    if (!movies.some((movie) => movie.id === Number(movieId))) {
-      return NextResponse.redirect(new URL('/movies', request.url));
+    if (!bikes.some((bike) => bike.id === Number(bikeid))) {
+      return NextResponse.redirect(new URL('/bikes', request.url));
     }
   }
 
@@ -27,5 +27,5 @@ export async function middleware (request: NextRequest) {
 }
 
 export const config = {
-  matcher: [ '/movies/:id*' ]
+  matcher: [ '/bikes/:id*' ]
 };
