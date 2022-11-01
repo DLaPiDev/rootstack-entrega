f19076ec211cc201ecc1da609976b11e1c35dd3c
DLaPiDev <114939794+DLaPiDev@users.noreply.github.com>
2022-11-01 12:23:51 -0300

    Update index.tsx

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,1 +1,13 @@
-export {}
+import Link from 'next/link'
+
+function Events() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Events
